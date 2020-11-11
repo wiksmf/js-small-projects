d468eb3920be9c9555f3de8aaf78a1ce0af94105
wiksmf <63299936+wiksmf@users.noreply.github.com>
2020-11-11 21:56:16 +0100

    day 11: initial upload

---
+++ Day 11 - Drum Kit/main.js
@@ -0,0 +1,41 @@
+document.addEventListener('keypress', (e) => {
+  playSound(e.key);
+  btnAnimation(e.key);
+});
+
+function playSound(key) {
+  switch (key) {
+    case 'a':
+      new Audio('sounds/tom1.mp3').play();
+      break;
+
+    case 'k':
+      new Audio('sounds/tom2.mp3').play();
+      break;
+
+    case 's':
+      new Audio('sounds/tom3.mp3').play();
+      break;
+
+    case 'l':
+      new Audio('sounds/snare.mp3').play();
+      break;
+
+    case 'j':
+      new Audio('sounds/crash.mp3').play();
+      break;
+
+    case 'd':
+      new Audio('sounds/bass.mp3').play();
+      break;
+  }
+};
+
+function btnAnimation(key) {
+  let activeBtn = document.querySelector("." + key);
+  activeBtn.classList.add("pressed");
+
+  setTimeout(() => {
+    activeBtn.classList.remove("pressed");
+  }, 100);
+};
