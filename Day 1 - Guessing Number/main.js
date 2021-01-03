75cb7c9c7381b77da0531db55d68d2a10423c9f2
wiksmf <63299936+wiksmf@users.noreply.github.com>
2021-01-03 23:08:18 +0100

    html, css, js update

--- Day 1 - Guessing Number/main.js
+++ Day 1 - Guessing Number/main.js
@@ -1,59 +1,70 @@
-// Constants to store references to the form text input and submit button
-const guessField = document.querySelector('.guessField');
-const submitBtn = document.querySelector('.submitBtn');
-const resetBtn = document.querySelector('.resetBtn');
+'use strict';
 
-// Constants to store a reference to the results paragraphs in the HTML
-const resetPar = document.querySelectorAll('.resultsParagraph p');
+const secretNumber = document.querySelector('.number');
+
+const chances = document.querySelector('.chances');
+const guessField = document.querySelector('.guess-field');
+const previousGuesses = document.querySelector('.previous-guesses');
 const guesses = document.querySelector('.guesses');
-const lastResult = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi');
 
-// Generate a random number between 1 and 100.
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+const message = document.querySelector('.message');
+const displayNumberRounds = document.querySelector('.rounds');
+const displayNumberWins = document.querySelector('.wins');
+const displayNumberLosses = document.querySelector('.losses');
 
-// Variable to keep track of how many guesses the player has had
-let guessCount = 1;
+const submitBtn = document.querySelector('.btn-guess');
+const resetBtn = document.querySelector('.btn-again');
 
-// Collect input from the user when the form is submitted
+let randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
+let chancesLeft = 10;
+let wins = 0;
+let losses = 0;
+
 submitBtn.addEventListener('click', checkGuess);
 
-
-// Function to check whether the guess is the correct number
+// Check whether the guess is the correct number
 function checkGuess() {
-  let userGuess = Number(guessField.value);
+  let userGuess = +guessField.value;
 
-  if (guessCount === 1) {
-    guesses.textContent = 'Previous guesses: ';
+  if (!userGuess) {
+    displayMessage('message', '⛔️ No number!');
+    return;
   }
-  guesses.textContent += userGuess + ' ';
 
   if (userGuess === randomNumber) {
-    lastResult.textContent = 'Congratulations! You got it right!';
-    lastResult.classList.remove('wrong');
-    lastResult.classList.add('correct');
-    lowOrHi.textContent = '';
+    wins++;
+    message.textContent = '🎉 congratulations! you got it right! 🎉';
+    displayNumberWins.textContent = wins;
+    secretNumber.textContent = randomNumber;
+    secretNumber.classList.add('guessed');
     setGameOver();
-  } else if (guessCount === 10) {
-    lastResult.textContent = '!!! GAME OVER !!!';
+  } else if (chancesLeft <= 1) {
+    losses++;
+    message.textContent = '😢 game over 😢';
+    displayNumberLosses.textContent = losses;
     setGameOver();
   } else {
-    lastResult.textContent = 'Wrong!';
-    lastResult.classList.add('wrong');
-    if (userGuess < randomNumber) {
-      lowOrHi.textContent = 'Last guess was too low!';
-    } else if (userGuess > randomNumber) {
-      lowOrHi.textContent = 'Last guess was too high!';
+    if (chancesLeft === 10) {
+      previousGuesses.classList.remove('hidden');
     }
+
+    message.textContent =
+      userGuess < randomNumber
+        ? `🔽 last guess was too low!`
+        : `🔼 last guess was too high!`;
   }
 
-  guessCount++;
+  chancesLeft--;
+
+  chances.textContent = chancesLeft;
+  guesses.textContent += `${userGuess} `;
+  displayNumberRounds.textContent = wins + losses;
+
   guessField.value = '';
   guessField.focus();
 }
 
-
-// Function to restart the game
+// Restart the game
 function setGameOver() {
   guessField.disabled = true;
   submitBtn.disabled = true;
@@ -61,24 +72,23 @@ function setGameOver() {
   resetBtn.addEventListener('click', resetGame);
 }
 
-
-// Function to reset the game logic and webpage
+// Reset the game logic and webpage
 function resetGame() {
   resetBtn.classList.add('hidden');
 
-  guessCount = 1;
-
-  for (let i = 0; i < resetPar.length; i++) {
-    resetPar[i].textContent = '';
-  }
-
   guessField.disabled = false;
   submitBtn.disabled = false;
+
+  previousGuesses.classList.add('hidden');
+  secretNumber.classList.remove('guessed');
+
+  randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
+  chancesLeft = 10;
+
+  message.textContent = '💬 start guessing . . .';
+  secretNumber.textContent = `?`;
+  chances.textContent = chancesLeft;
+  guesses.textContent = '';
+
   guessField.value = '';
-  guessField.focus();
-
-  lastResult.classList.remove('correct');
-  lastResult.classList.remove('wrong');
-
-  randomNumber = Math.floor(Math.random() * 100) + 1;
 }
