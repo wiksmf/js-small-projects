1ac519b3144abaeb311d6e5522c19fe97b832f1c
wiksmf <wiksmf@gmail.com>
2021-01-12 20:16:06 +0100

    html, css, js update

--- Armstrong number/main.js
+++ Armstrong number/main.js
@@ -1,26 +1,63 @@
 'use strict';
 
-const userInput = document.querySelector('.number');
+const userInput = document.querySelector('.input__number');
 const btnCheck = document.querySelector('button');
-const result = document.querySelector('.result');
+const displayResult = document.querySelector('.result');
+const displayInfo = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
-
-btnCheck.addEventListener('click', (e) => {
+btnCheck.addEventListener('click', e => {
   e.preventDefault();
 
-  let input = Number(userInput.value);
+  // Check whether the input is a positive number
+  if (userInput.value === '' || userInput.value < 0) {
+    displayResult.textContent = '⚠️ invalid input';
+    return;
+  }
 
-  if (isNaN(input) || (input === '')) return;
+  displayResult.textContent = `
+  ${userInput.value}
+  ${
+    isArmstrong(userInput.value)
+      ? 'is an Armstrong Number 😁'
+      : 'is not an Armstrong Number 😔'
+  }  
+  `;
 
-  result.textContent = `${isArmstrongNumber(input) ? 'Armstrong number!' : ' Not Armstrong number!'}`;
+  userInput.value = '';
+  userInput.focus();
 });
 
+// Check if the number is an Armstrong number
+function isArmstrong(number) {
+  // Count the number of digits
+  const digits = number.length;
 
-function isArmstrongNumber(input) {
+  // Convert the number into an array
+  const numberArray = Array.from(String(number));
 
-  const number = Array.from(String(input), Number);
-  const numberOfDigits = number.length;
-  const exponents = number.map(num => Math.pow(num, numberOfDigits));
+  // Raise each digit to the power of the number of digits
+  const exponents = numberArray.map(numb => numb ** digits);
 
-  return exponents.reduce((a, b) => a + b, 0) === input;
+  // Sum the digits
+  const sum = exponents.reduce((a, b) => a + b);
+
+  return +number === sum;
 }
+
+// Handling information modal
+btnOpenModal.addEventListener('click', () => {
+  displayInfo.classList.remove('hidden');
+});
+
+btnCloseModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    displayInfo.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !btnCloseModal.classList.contains('hidden'))
+    displayInfo.classList.add('hidden');
+});
