56ef5a5263980cb79b2ee06e861151dd2bef7567
wiksmf <wiksmf@gmail.com>
2021-01-17 21:56:26 +0100

    html, css, js update

--- Factorial!/main.js
+++ Factorial!/main.js
@@ -1,27 +1,44 @@
 'use strict';
 
-const userInput = document.querySelector('.number');
-const btnCheck = document.querySelector('button');
+const userInput = document.querySelector('.form__input');
+const btnCalc = document.querySelector('.btn');
 const result = document.querySelector('.result');
+const displayInfo = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
-
-btnCheck.addEventListener('click', (e) => {
+btnCalc.addEventListener('click', e => {
   e.preventDefault();
 
-  let number = +userInput.value;
-
-  if (isNaN(number) || (number === '')) return;
-
-  if (number <= 0) result.textContent = `it must be a positive integer`;
-  else factorial(number);
+  if (!userInput.value && userInput.value <= 0)
+    result.textContent = `⚠️ it must be a positive integer`;
+  else factorial(userInput.value);
 });
-
 
 function factorial(number) {
   let product = 1;
+  result.textContent = `${number}! = `;
+
   for (let i = 1; i <= number; i++) {
-      product *= i;
+    product *= i;
+    result.textContent += `${i < number ? `${i} * ` : `${i} = `}`;
   }
 
-  result.textContent = `${(number)} ! is ${product}`;
+  result.textContent += `${product}`;
 }
+
+// Handling information modal
+btnOpenModal.addEventListener('click', () => {
+  displayInfo.classList.remove('hidden');
+});
+
+btnCloseModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    displayInfo.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !btnCloseModal.classList.contains('hidden'))
+    displayInfo.classList.add('hidden');
+});
