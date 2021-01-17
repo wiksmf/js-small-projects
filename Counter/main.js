f8b5f9da968dd5b1927e3bdaf76a2ac752dbdbb7
wiksmf <wiksmf@gmail.com>
2021-01-17 15:56:58 +0100

    html, css, js update

--- Counter/main.js
+++ Counter/main.js
@@ -1,27 +1,21 @@
 'use strict';
 
-const buttons = document.querySelectorAll('button');
-const displayCount = document.querySelector('p');
+const buttons = document.querySelectorAll('.btn');
+const displayCount = document.querySelector('.paragraph');
 
 let counter = 0;
 
-
-buttons.forEach((btn) => {
+// Add or subtract the value
+buttons.forEach(btn => {
   btn.addEventListener('click', () => {
-
-    if (btn.classList.contains('add')) {
-      counter++;
-    }
-
-    if (btn.classList.contains('subtract')) {
-      counter--;
-    }
+    if (btn.classList.contains('add')) counter++;
+    if (btn.classList.contains('subtract')) counter--;
 
     updateCounter();
   });
 });
 
-
+// Update the counter
 function updateCounter() {
   displayCount.textContent = `${counter}`;
   displayCount.classList.toggle('negative-value', counter < 0);
