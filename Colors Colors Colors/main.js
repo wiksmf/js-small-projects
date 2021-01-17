94acb35f65a5d436ad0e4d6d3740fbb0bdbc5e44
wiksmf <wiksmf@gmail.com>
2021-01-17 14:34:53 +0100

    html, css, js update

--- Colors Colors Colors/main.js
+++ Colors Colors Colors/main.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const body = document.querySelector('body');
-const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
+const container = document.querySelector('.container ');
+const heading = document.querySelector('.heading-primary');
+const paragraph = document.querySelector('.paragraph');
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-
-document.addEventListener('mousemove', (e) => {
+document.addEventListener('mousemove', e => {
   const x = e.clientX;
   const y = e.clientY;
 
@@ -15,15 +14,13 @@ document.addEventListener('mousemove', (e) => {
   const green = Math.trunc((y / height) * 100);
   const blue = Math.trunc((red + green) / 2);
 
-  var color = red + green + blue;
+  const color = red + green + blue;
 
-  h1.style.color = setTextColor(color);
-  p.style.color = setTextColor(color);
-
-  body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`;
+  heading.style.color = setTextColor(color);
+  paragraph.style.color = setTextColor(color);
+  container.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`;
 });
 
-
 function setTextColor(color) {
-  return (color > 60) ? `rgb(0, 0, 0)` : `rgb(255,255, 255)`;
+  return color > 60 ? `rgb(0, 0, 0)` : `rgb(255,255, 255)`;
 }
