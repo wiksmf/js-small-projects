c22346d3da8f155325114956c2d48fca098d58ac
wiksmf <wiksmf@gmail.com>
2021-01-19 19:58:17 +0100

    html, css, js update

--- Happy Numbers/main.js
+++ Happy Numbers/main.js
@@ -1,36 +1,69 @@
 'use strict';
 
-const userInput = document.querySelector('.number');
-const btnCheck = document.querySelector('button');
+const userInput = document.querySelector('.form__input');
+const btnCheck = document.querySelector('.btn');
 const result = document.querySelector('.result');
+const displayInfo = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
-btnCheck.addEventListener('click', (e) => {
+btnCheck.addEventListener('click', e => {
   e.preventDefault();
 
-  let input = userInput.value;
-
-  if (isNaN(input) || (input === '')) return;
-
-  for (let i = 0; i < 100; i++) {
-    input = isHappyNumber(input);
-
-    if (input === 1) {
-      result.textContent = `Happy Number ☺!`;
-    } else {
-      result.textContent = `Not Happy Number ☹!`;
-    }
+  if (!userInput.value || userInput.value <= 0) {
+    result.textContent = `⚠️ it must be a positive integer`;
+    resetInput();
+    return;
   }
+
+  result.textContent = `${userInput.value} is ${
+    isHappyNumber(+userInput.value)
+      ? `a Happy Number 😃!`
+      : `an Unhappy Number 😢!`
+  }`;
+
+  resetInput();
 });
 
+// Check if it's a happy number
+function isHappyNumber(number) {
+  // Convert the number into an array
+  const numberArray = Array.from(String(number));
 
-function isHappyNumber(input) {
-  let temp;
-  let sum = 0;
+  // Get the square of each digit
+  const exponents = numberArray.map(num => num ** 2);
 
-  while (input !== 0) {
-    temp = input % 10;
-    sum += (temp * temp);
-    input = (input - temp) / 10
-  }
-  return sum;
+  // Sum the squares
+  const happyNumber = exponents.reduce((a, b) => a + b);
+
+  // If the result is equal to 1, it's a happy number
+  // if the result is equal to 4, it's an unhappy number
+  // else repeat the process
+  return happyNumber === 1
+    ? true
+    : happyNumber === 4
+    ? false
+    : isHappyNumber(happyNumber);
 }
+
+// Reset input field and sequence array
+function resetInput() {
+  userInput.value = '';
+  userInput.focus();
+}
+
+// Handling information modal
+btnOpenModal.addEventListener('click', () => {
+  displayInfo.classList.remove('hidden');
+});
+
+btnCloseModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    displayInfo.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !btnCloseModal.classList.contains('hidden'))
+    displayInfo.classList.add('hidden');
+});
