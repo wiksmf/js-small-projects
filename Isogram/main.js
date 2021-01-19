334c7f0358033990faa9eeff9a758144bbb92741
wiksmf <wiksmf@gmail.com>
2021-01-19 21:31:43 +0100

    html, css, js update

--- Isogram/main.js
+++ Isogram/main.js
@@ -1,28 +1,50 @@
 'use strict';
 
-const userInput = document.querySelector('input');
-const btnCheck = document.querySelector('button');
+const userInput = document.querySelector('.input__word');
+const btnCheck = document.querySelector('.btn-check');
 const result = document.querySelector('.result');
+const displayInfo = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
-btnCheck.addEventListener('click', (e) => {
+btnCheck.addEventListener('click', e => {
   e.preventDefault();
 
   let string = userInput.value;
 
-  if (string === '') return;
+  if (!userInput.value) {
+    result.textContent = '⚠️ invalid input';
+    return;
+  }
 
-  isIsogram(string) ?
-    result.textContent = 'isogram!' :
-    result.textContent = 'not isogram!';
+  result.textContent = `
+   ${isIsogram(string) ? 'isogram 🤭!' : 'not isogram 😓!'};
+  `;
 });
 
+// Check if the input is isogram
 function isIsogram(string) {
   string = string.replace(/([^A-Za-z])/g, '');
   string = string.toLowerCase().split('');
 
   return (
-    string.filter(
-      (letter, index) => string.indexOf(letter) === index
-    ).length === string.length
+    string.filter((letter, index) => string.indexOf(letter) === index)
+      .length === string.length
   );
 }
+
+// Handling information modal
+btnOpenModal.addEventListener('click', () => {
+  displayInfo.classList.remove('hidden');
+});
+
+btnCloseModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    displayInfo.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !btnCloseModal.classList.contains('hidden'))
+    displayInfo.classList.add('hidden');
+});
