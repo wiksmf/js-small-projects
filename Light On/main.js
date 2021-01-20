068591f088e70229d5987caa294406b83f940637
wiksmf <wiksmf@gmail.com>
2021-01-20 18:05:17 +0100

    html, css, js update

--- Light On/main.js
+++ Light On/main.js
@@ -3,11 +3,10 @@
 const btnChangeBulb = document.querySelectorAll('button');
 const bulb = document.querySelectorAll('div');
 
-
-btnChangeBulb.forEach((btn) => {
+btnChangeBulb.forEach(btn => {
   btn.addEventListener('click', () => {
-    bulb.forEach((bulb) => {
-      bulb.classList.toggle('hidden')
+    bulb.forEach(bulb => {
+      bulb.classList.toggle('hidden');
     });
   });
 });
