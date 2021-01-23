671ba6388bc2b830d854a6743f9aa3ccff820af2
wiksmf <wiksmf@gmail.com>
2021-01-23 21:35:31 +0100

    html, css, js update

--- Pyramid/main.js
+++ Pyramid/main.js
@@ -1,28 +1,24 @@
 'use strict';
 
 const userInput = document.querySelector('#number');
-const btnCheck = document.querySelector('button');
-const result = document.querySelector('p');
+const btn = document.querySelector('button');
+const pyramid = document.querySelector('p');
 
-btnCheck.addEventListener('click', e => {
-  result.innerHTML = ``;
+btn.addEventListener('click', () => {
+  pyramid.innerHTML = ``;
 
-  e.preventDefault();
-
-  let input = userInput.value;
-
-  if (!input) {
-    result.textContent = 'enter a number';
+  if (!userInput.value) {
+    pyramid.textContent = '⚠️ please enter a number';
     return;
   }
-  createPyramid(input);
+  createPyramid(userInput.value);
 });
 
 function createPyramid(rows) {
   for (var i = 1; i <= rows; i++) {
     for (var j = 1; j <= i; j++) {
-      result.innerHTML += `* `;
+      pyramid.innerHTML += `* `;
     }
-    result.innerHTML += `<br/>`;
+    pyramid.innerHTML += `<br/>`;
   }
 }
