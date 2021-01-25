31a6bd68267b10ed5f69b11b2f49d0a62e53c529
wiksmf <wiksmf@gmail.com>
2021-01-26 00:07:31 +0100

    html, css, js update

--- Triangles/main.js
+++ Triangles/main.js
@@ -5,24 +5,42 @@ const b = document.querySelector('.b');
 const c = document.querySelector('.c');
 const btnCheck = document.querySelector('button');
 const result = document.querySelector('.result');
+const displayInfo = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
-
-btnCheck.addEventListener('click', (e) => {
+btnCheck.addEventListener('click', e => {
   e.preventDefault();
 
   const sideA = Number(a.value);
   const sideB = Number(b.value);
   const sideC = Number(c.value);
 
-  if ((sideA < (sideB + sideC)) && (sideB < (sideA + sideC)) && (sideC < (sideA + sideB))) {
-    if ((sideA === sideB) && (sideB === sideC)) {
+  if (sideA < sideB + sideC && sideB < sideA + sideC && sideC < sideA + sideB) {
+    if (sideA === sideB && sideB === sideC) {
       result.textContent = 'equilateral triangle';
-    } else if ((sideB === sideC) || (sideA === sideC)) {
+    } else if (sideB === sideC || sideA === sideC) {
       result.textContent = 'isosceles triangle';
-    } else if ((sideA !== sideB) && (sideB !== sideC) && (sideA !== sideC)) {
+    } else if (sideA !== sideB && sideB !== sideC && sideA !== sideC) {
       result.textContent = 'scalene triangle';
     }
   } else {
     result.textContent = 'not a triangle';
   }
+});
+
+// Handling information modal
+btnOpenModal.addEventListener('click', () => {
+  displayInfo.classList.remove('hidden');
+});
+
+btnCloseModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    displayInfo.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !btnCloseModal.classList.contains('hidden'))
+    displayInfo.classList.add('hidden');
 });
