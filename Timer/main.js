e549f29fa82968bd35142ac66ddf0925dfcfb2f1
wiksmf <wiksmf@gmail.com>
2021-01-28 18:12:30 +0100

    html, css, js update

--- Timer/main.js
+++ Timer/main.js
@@ -1,23 +1,19 @@
 'use strict';
 
-const userInput = document.querySelector('input');
-const btnStart = document.querySelector('.play');
-
 const buttons = document.querySelectorAll('button');
 
 const displayHours = document.querySelector('.display-hours');
 const displayMinutes = document.querySelector('.display-minutes');
 const displaySeconds = document.querySelector('.display-seconds');
 
-
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
+let totalTime, timeLeft, timer;
 
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
-
-    if (btn.classList.contains('hours-plus')  && (hours < 24)) hours++;
+    if (btn.classList.contains('hours-plus') && (hours < 24)) hours++;
     if (btn.classList.contains('hours-minus') && (hours > 0)) hours--;
 
     if (btn.classList.contains('minutes-plus') && (minutes < 59)) minutes++;
@@ -26,13 +22,44 @@ buttons.forEach(btn => {
     if (btn.classList.contains('seconds-plus') && (seconds < 59)) seconds++;
     if (btn.classList.contains('seconds-minus') && (seconds > 0)) seconds--;
 
-    console.log(hours, minutes, seconds);
-    updateUI();
+    if (btn.classList.contains('play')) startTimer();
+    if (btn.classList.contains('pause')) pauseTimer();
+    if (btn.classList.contains('restart')) restartTimer();
+
+    updateUI(hours, minutes, seconds);
   })
 });
 
-function updateUI() {
-  displayHours.textContent = `${hours < 10 ? `0${hours}` : `${hours}`}`;
-  displayMinutes.textContent = `${minutes < 10 ? `0${minutes}` : `${minutes}`}`;
-  displaySeconds.textContent = `${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
+
+function startTimer() {
+  totalTime = (hours * 60 * 60) + (minutes * 60) + seconds;
+  if (!totalTime) return;
+
+  timer = setInterval(() => {
+    timeLeft = --totalTime;
+    formatTime(timeLeft);
+
+    if (timeLeft <= 0) clearInterval(timer);
+  }, 1000);
+}
+
+function pauseTimer() {
+}
+
+function restartTimer() {
+  clearInterval(timer)
+}
+
+function formatTime(timeLeft) {
+  const hoursLeft = Math.floor((timeLeft / 3600) % 24);
+  const minutesLeft = Math.floor((timeLeft / 60) % 60)
+  const secondsLeft = Math.floor(timeLeft % 60);
+
+  updateUI(hoursLeft, minutesLeft, secondsLeft);
+}
+
+function updateUI(hours, minutes, seconds) {
+  displayHours.textContent = hours < 10 ? `0${hours}` : hours;
+  displayMinutes.textContent = minutes < 10 ? `0${minutes}` : minutes;
+  displaySeconds.textContent = seconds < 10 ? `0${seconds}` : seconds;
 }
