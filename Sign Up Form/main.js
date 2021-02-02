8df3e94a7d26e13998091064be91c619a14b6dd3
wiksmf <wiksmf@gmail.com>
2021-02-02 22:12:22 +0100

    html, css, js update

--- Sign Up Form/main.js
+++ Sign Up Form/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const form = document.querySelector('.form');
 const firstName = document.querySelector('.first-name');
 const lastName = document.querySelector('.last-name');
@@ -6,43 +8,29 @@ const pwd = document.querySelector('.pwd');
 
 const regEx = {
   email: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-  pwd: /^[A-Za-z0-9]\w{8,16}$/
-}
+  pwd: /^(?=.*\d)(?=.*[a-zA-Z])\w{8,14}$/,
+};
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  if(firstName.value === "") {
-    displayError(firstName);
-  } else {
-    removeError(firstName);
-  }
-  
-  if (lastName.value === ""){
-    displayError(lastName);
-  } else {
-    removeError(lastName);
-  }
-  
-  if (!regEx.email.test(email.value)) {
-    displayError(email);
-  } else {
-    removeError(email);
-  }
+  !firstName.value ? displayError(firstName) : removeError(firstName);
+  !lastName.value ? displayError(lastName) : removeError(lastName);
+  !regEx.email.test(email.value) ? displayError(email) : removeError(email);
+  !regEx.pwd.test(pwd.value) ? displayError(pwd) : removeError(pwd);
 
-  if(!regEx.pwd.test(pwd.value)){
-    displayError(pwd);
-  }else {
-    removeError(pwd);
-  }
+  firstName.value = '';
+  lastName.value = '';
+  email.value = '';
+  pwd.value = '';
 });
 
-function displayError(inputItem){
+function displayError(inputItem) {
   inputItem.classList.add('error-input');
   inputItem.nextElementSibling.classList.remove('hidden');
 }
 
-function removeError(inputItem){
+function removeError(inputItem) {
   inputItem.classList.remove('error-input');
   inputItem.nextElementSibling.classList.add('hidden');
 }
