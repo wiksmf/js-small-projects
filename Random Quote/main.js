32c2af0c70445fed80cf648198878746e9d74961
wiksmf <wiksmf@gmail.com>
2021-02-02 01:09:33 +0100

    html, css, js update

--- Random Quote/main.js
+++ Random Quote/main.js
@@ -4,11 +4,19 @@ const displayQuote = document.querySelector('.text');
 const displayAuthor = document.querySelector('.author');
 const url = `https://type.fit/api/quotes`;
 
-fetch(url)
-  .then((res) => res.json())
-  .then((quote) => {
-    let max = quote.length;
-    let randomNumber = Math.floor(Math.random() * max + 1);
+// Get random quote
+async function getQuote() {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) throw new Error(`Whoops.. there are some problems 😥`);
+
+    const quote = await res.json();
+    const randomNumber = Math.floor(Math.random() * quote.length + 1);
     displayQuote.textContent = `${quote[randomNumber].text}`;
     displayAuthor.textContent = `${quote[randomNumber].author}`;
-  });
+  } catch (err) {
+    alert(`💥 ${err}`);
+  }
+}
+
+getQuote();
