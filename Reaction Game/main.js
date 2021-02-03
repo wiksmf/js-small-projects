2090f46abbcd5865960ea49bc8386b8f69910505
wiksmf <wiksmf@gmail.com>
2021-02-03 21:04:49 +0100

    js update

--- Reaction Game/main.js
+++ Reaction Game/main.js
@@ -1,77 +1,55 @@
+'use strict';
+
 const spinnerContainer = document.querySelector('.spinner');
 const spinner = document.querySelector('.spinner p');
-const btn = document.querySelector('button');
+const btnStart = document.querySelector('.btn');
 const result = document.querySelector('.result');
 
 let rotateCount = 0;
 let startTime = null;
 let requestAF;
 
-
 // Handle the start button when clicked
-btn.addEventListener('click', start);
-
+btnStart.addEventListener('click', startGame);
 
 // Start the spinner and the game
-function start() {
-  draw();
-
-  btn.classList.add('hidden');
+function startGame() {
+  result.classList.add('visible');
+  btnStart.classList.add('visible');
   spinnerContainer.classList.remove('visible');
-
+  animateSpinner();
   setTimeout(endGame, randomNumber(5000, 10000));
 }
 
-
 // Animates the spinner
-function draw(timestamp) {
-  if (!startTime) {
-    startTime = timestamp;
-  }
+function animateSpinner(timestamp) {
+  if (!startTime) startTime = timestamp;
 
   rotateCount = (timestamp - startTime) / 3;
 
-  if (rotateCount > 359) {
-    rotateCount %= 360;
-  }
+  if (rotateCount > 359) rotateCount %= 360;
 
   spinner.style.transform = `rotate(${rotateCount}deg)`;
-  requestAF = requestAnimationFrame(draw);
+  requestAF = requestAnimationFrame(animateSpinner);
 }
-
 
 // End the game
 function endGame() {
   cancelAnimationFrame(requestAF);
-
-  spinnerContainer.classList.add('visible')
-  result.classList.remove('hidden');
+  spinnerContainer.classList.add('visible');
+  result.classList.remove('visible');
   result.textContent = 'PLAYERS GO!!!';
-
   document.addEventListener('keydown', clickHandler);
 }
 
-
 // Check which user win the game
 function clickHandler(e) {
-  if (e.key === 'a' || e.key === 'A') {
-    result.textContent = `Player 1 won!🚩`;
-  } else if (e.key === 'l' || e.key === 'L') {
-    result.textContent = `Player 2 won! 🚩`;
-  }
+  if (e.key === 'a' || e.key === 'A') result.textContent = `Player 1 won!🚩`;
+  if (e.key === 'l' || e.key === 'L') result.textContent = `Player 2 won!🚩`;
 
   document.removeEventListener('keydown', clickHandler);
-  setTimeout(resetGame, 5000);
+  setTimeout(() => btnStart.classList.remove('visible'), 2500);
 }
-
-
-// Set the game back to the original state
-function resetGame() {
-  btn.classList.remove('hidden');
-  result.classList.add('hidden');
-  result.textContent = '';
-}
-
 
 // Return a random number between two given values
 function randomNumber(min, max) {
