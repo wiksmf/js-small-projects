a801f027cee8784f8ccce8b999c59c99487d516e
wiksmf <wiksmf@gmail.com>
2021-02-03 23:03:13 +0100

    html, css, js update

--- Pythagorean Theorem/main.js
+++ Pythagorean Theorem/main.js
@@ -2,24 +2,46 @@
 
 const sideA = document.querySelector('.side-a');
 const sideB = document.querySelector('.side-b');
-const btnCheck = document.querySelector('button');
+const btnCheck = document.querySelector('.btn');
 const displayResult = document.querySelector('.result');
+
+const displayInfo = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
 btnCheck.addEventListener('click', (e) => {
   e.preventDefault();
 
-  let a = +sideA.value;
-  let b = +sideB.value;
+  if (!sideA.value) return;
+  if (!sideB.value) return;
 
-  if (isNaN(a) || a === '') return;
-  if (isNaN(b) || b === '') return;
-
-  displayResult.textContent = `${pythagorean(a, b)}`;
+  displayResult.innerHTML = `
+    √(${sideA.value}<sup>2</sup> + ${sideB.value}<sup>2</sup>) =  
+    ${pythagorean(sideA.value, sideB.value)}
+  `;
 
   sideA.value = '';
   sideB.value = '';
 });
 
+// Calc the sum of the squares of the other two sides
 function pythagorean(sideA, sideB) {
-  return Math.sqrt(sideA ** 2 + sideB ** 2);
+  return Math.sqrt(sideA ** 2 + sideB ** 2).toFixed(2);
 }
+
+
+// Handling information modal
+btnOpenModal.addEventListener('click', () => {
+  displayInfo.classList.remove('hidden');
+});
+
+btnCloseModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    displayInfo.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !btnCloseModal.classList.contains('hidden'))
+    displayInfo.classList.add('hidden');
+});
