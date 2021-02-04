7c6888676ea2093f17d8640e7437561afcbb330b
wiksmf <wiksmf@gmail.com>
2021-02-04 21:25:53 +0100

    html, css, js update

--- Prime Number/main.js
+++ Prime Number/main.js
@@ -1,32 +1,53 @@
 'use strict';
 
-const userInput = document.querySelector('.number');
+const userInput = document.querySelector('.input-data');
 const btnCheck = document.querySelector('button');
 const result = document.querySelector('.result');
 
+const displayInfo = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
-btnCheck.addEventListener('click', (e) => {
+btnCheck.addEventListener('click', e => {
   e.preventDefault();
 
-  let number = Number(userInput.value);
-
-  if (isNaN(number) || (number === '')) return;
-
-  if (number <= 1) {
-    result.textContent = `it must be a number greater than 1`;
-  } else {
-    isPrime(number);
+  if (!userInput.value || userInput.value <= 1) {
+    result.textContent = `⚠️ it must be a positive integer greater than 1`;
+    resetInput();
+    return;
   }
+
+  result.textContent = `🧮 it ${
+    isPrime(userInput.value) ? 'is' : 'is not'
+  } a prime number`;
+  resetInput();
 });
 
-
 function isPrime(number) {
-  let prime = true;
   for (let i = 2; i < number; i++) {
-    if (number % i === 0) {
-      prime = false;
-    }
+    if (number % i === 0) return false;
   }
-
-  result.textContent = `it ${(prime === true) ? 'is' : 'is not'} a prime number`;
+  return true;
 }
+
+// Reset input field and sequence array
+function resetInput() {
+  userInput.value = '';
+  userInput.focus();
+}
+
+// Handling information modal
+btnOpenModal.addEventListener('click', () => {
+  displayInfo.classList.remove('hidden');
+});
+
+btnCloseModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    displayInfo.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !btnCloseModal.classList.contains('hidden'))
+    displayInfo.classList.add('hidden');
+});
