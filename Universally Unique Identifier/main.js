6adcc35f99cc037f41587d747fb34de96a958ccc
wiksmf <wiksmf@gmail.com>
2021-02-06 17:00:10 +0100

    html, css, js update

--- Universally Unique Identifier/main.js
+++ Universally Unique Identifier/main.js
@@ -1,16 +1,25 @@
 'use strict';
 
-const btnCreate = document.querySelector('button');
-const displayResult = document.querySelector('.result');
+const btnCreate = document.querySelector('.btn-create');
+const btnCopy = document.querySelector('.btn-copy');
+const displayUUID = document.querySelector('.uuid');
+
+const id = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx';
 
 btnCreate.addEventListener('click', () => {
-
   let dayTime = new Date().getTime();
-  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (char) {
+
+  const uuid = id.replace(/[x]/g, char => {
     const randomValue = (dayTime + Math.random() * 16) % 16 | 0;
     dayTime = Math.floor(dayTime / 16);
-    return (char == 'x' ? randomValue : (randomValue & 0x3 | 0x8)).toString(16);
+
+    return (char == 'x' ? randomValue : (randomValue & 0x3) | 0x8).toString(16);
   });
 
-  displayResult.textContent = uuid;
+  displayUUID.value = uuid;
+});
+
+btnCopy.addEventListener('click', () => {
+  displayUUID.select();
+  document.execCommand('copy');
 });
