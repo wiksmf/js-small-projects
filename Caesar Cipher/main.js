7c92423b1d7d75ae8ac5b0cb894c3570a9f2e75a
wiksmf <wiksmf@gmail.com>
2021-02-06 23:30:06 +0100

    html, css, js update

--- Caesar Cipher/main.js
+++ Caesar Cipher/main.js
@@ -1,48 +1,65 @@
 'use strict';
 
-const userInput = document.querySelector('input');
-const btnCheck = document.querySelector('button');
+const string = document.querySelector('.string-input');
+const key = document.querySelector('.key-input');
+const btnCheck = document.querySelector('.btn-encrypt');
 const result = document.querySelector('.result');
 
-const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+const displayInfo = document.querySelector('.modal');
+const btnOpenModal = document.querySelector('.open-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 
-btnCheck.addEventListener('click', (e) => {
+btnCheck.addEventListener('click', e => {
   e.preventDefault();
 
-  let string = userInput.value;
+  if (!string.value || !key.value || key.value <= 0) {
+    result.textContent =
+      '⚠️ invalid string or key (key must be greater or equal to 1)';
+    return;
+  }
 
-  if (string === '') return;
-
-  caesarCipher(string);
+  result.textContent = caesarCipher(string.value, +key.value);
 });
 
-function caesarCipher(string) {
+// Encrypt the string according to the shift key
+function caesarCipher(string, key) {
   let output = '';
-  let key = 5;
 
   for (let i = 0; i < string.length; i++) {
-    let index = alphabet.indexOf(string[i].toLowerCase());
-    let shift = index + key;
-    let notLetter = string[i].match(/[^a-zA-Z]/g);
-
-    if (shift >= 26) {
-      shift = key - (alphabet.length - index);
-      output += isUpper(string, i, shift);
+    if (string[i].match(/[a-zA-Z]/g)) {
+      if (isUpper(string[i]))
+        output += String.fromCharCode(
+          ((string.charCodeAt(i) + key - 65) % 26) + 65,
+        );
+      else
+        output += String.fromCharCode(
+          ((string.charCodeAt(i) + key - 97) % 26) + 97,
+        );
     } else {
-      if (notLetter) {
-        output += string[i];
-      } else {
-        output += isUpper(string, i, shift);
-      }
+      output += string[i];
     }
   }
-  result.textContent = output;
+
+  return output;
 }
 
-function isUpper(string, i, shift) {
-  if (string[i].match(/[A-Z]/g)) {
-    return (alphabet[shift]).toUpperCase();
-  } else {
-    return alphabet[shift];
-  }
+// Check if the letter is uppercase
+function isUpper(string) {
+  return string === string.toUpperCase();
 }
+
+// Handling information modal
+btnOpenModal.addEventListener('click', () => {
+  displayInfo.classList.remove('hidden');
+});
+
+btnCloseModal.forEach(btn => {
+  btn.addEventListener('click', () => {
+    displayInfo.classList.add('hidden');
+  });
+});
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'Escape' && !btnCloseModal.classList.contains('hidden'))
+    displayInfo.classList.add('hidden');
+});
