df497850e3f8c8db368d6c297de343aef8159b4a
wiksmf <wiksmf@gmail.com>
2021-02-10 00:50:39 +0100

    html, css, js update

--- Random Bees/main.js
+++ Random Bees/main.js
@@ -1,52 +1,79 @@
 'use strict';
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 const windowHeight = window.innerHeight - 100;
 const windowWidth = window.innerWidth - 100;
+
+let bees = [];
 
 canvas.width = windowWidth;
 canvas.height = windowHeight;
 
-let x = randomNumber(1, windowWidth);
-let y = randomNumber(1, windowHeight);
-
-setInterval(function () {
-  ctx.clearRect(0, 0, windowWidth, windowHeight);
-
-  for (let i = 0; i < randomNumber(1, 20); i++) {
-    drawBee(randomNumber(1, windowWidth), randomNumber(1, windowWidth));
+// Bee constructor
+class Bee {
+  constructor(x, y, velocityX, velocityY) {
+    this.x = x;
+    this.y = y;
+    this.velocityX = velocityX;
+    this.velocityY = velocityY;
   }
 
-  x = update(x);
-  y = update(y);
-}, 350);
+  // Bee draw method
+  drawBee() {
+    circle(this.x, this.y, 16, 'yellow');
+    circle(this.x, this.y, 16, 'white');
+    circle(this.x - 10, this.y - 22, 10, 'white');
+    circle(this.x + 10, this.y - 22, 10, 'black');
+    circle(this.x - 4, this.y - 2, 4, 'black');
+    circle(this.x + 4, this.y - 2, 4, 'black');
+  }
 
-const circle = function (x, y, r, color) {
+  // Bee update position method
+  updatePosition() {
+    if (this.x >= windowWidth || this.x <= 0) this.velocityX = -this.velocityX;
+    if (this.y >= windowHeight || this.y <= 0) this.velocityY = -this.velocityY;
+
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+  }
+}
+
+// Draw the circles
+function circle(x, y, r, color) {
   ctx.beginPath();
-  ctx.arc(x, y, r, 0, Math.PI * 2, false);
+  ctx.arc(x, y, r, Math.PI * 2, false);
   ctx.fill();
   ctx.fillStyle = color;
 }
 
-const drawBee = function (x, y) {
-  circle(x, y, 16, 'yellow');
-  circle(x, y, 16, 'white');
-  circle(x - 10, y - 22, 10, 'white');
-  circle(x + 10, y - 22, 10, 'black');
-  circle(x - 4, y - 2, 4, 'black');
-  circle(x + 4, y - 2, 4, 'black');
-}
-
-const update = function (x, y) {
-  coords += randomNumber(1, 10);
-
-  if (coords > windowWidth) coords = windowWidth;
-  if (coords < 0) coords = 0;
-
-  return coords;
-}
-
-function randomNumber(min, max) {
+// Generate random number between a range of two numbers
+function random(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+// Add bees to the canvas and animate them
+while (bees.length <= 20) {
+  let bee = new Bee(
+    random(1, windowWidth),
+    random(1, windowWidth),
+    random(-4, 4),
+    random(-4, 4),
+  );
+
+  bees.push(bee);
+}
+
+// Loop through all the bees and draw each one on the screen
+function loop() {
+  ctx.clearRect(0, 0, windowWidth, windowHeight);
+
+  bees.forEach(bee => {
+    bee.drawBee();
+    bee.updatePosition();
+  });
+
+  requestAnimationFrame(loop);
+}
+
+loop();
