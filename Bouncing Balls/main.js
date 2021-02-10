ffa2d50e4f244b7b034fceaac9f37118e5c8c9d5
wiksmf <wiksmf@gmail.com>
2021-02-10 21:15:21 +0100

    html, css, js update

--- Bouncing Balls/main.js
+++ Bouncing Balls/main.js
@@ -3,49 +3,58 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const width = window.innerWidth - 100;
+const height = window.innerHeight - 100;
+
+canvas.width = width;
+canvas.height = height;
 
 let balls = [];
 
+// Get random number between a range of two numbers
 function random(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function Ball(x, y, velocityX, velocityY, color, size) {
-  this.x = x;
-  this.y = y;
-  this.velocityX = velocityX;
-  this.velocityY = velocityY;
-  this.color = color;
-  this.size = size;
+// Ball constructor
+class Ball {
+  constructor(x, y, velocityX, velocityY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velocityX = velocityX;
+    this.velocityY = velocityY;
+    this.color = color;
+    this.size = size;
+  }
+
+  // Ball draw method
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+  // Ball update position method
+  updatePosition() {
+    if (this.x + this.size >= width || this.x - this.size <= 0)
+      this.velocityX = -this.velocityX;
+
+    if (this.y + this.size >= height || this.y - this.size <= 0)
+      this.velocityY = -this.velocityY;
+
+    this.x += this.velocityX;
+    this.y += this.velocityY;
+  }
 }
 
-Ball.prototype.draw = function () {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-};
-
-Ball.prototype.update = function () {
-  if (this.x + this.size >= width || this.x - this.size <= 0) {
-    this.velocityX = -this.velocityX;
-  }
-  if (this.y + this.size >= height || this.y - this.size <= 0) {
-    this.velocityY = -this.velocityY;
-  }
-
-  this.x += this.velocityX;
-  this.y += this.velocityY;
-};
-
+// Draw the balls
 function createBall() {
-  let size = random(25, 50);
+  const size = random(1, 50);
 
   return new Ball(
-    random(0 + size, width - size),
-    random(0 + size, height - size),
+    random(0 + size, canvas.width - size),
+    random(0 + size, canvas.height - size),
     random(-7, 7),
     random(-7, 7),
     `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`,
@@ -53,26 +62,28 @@ function createBall() {
   );
 }
 
-function loop() {
-  while (balls.length < 5) {
-    let ball = createBall();
-    balls.push(ball);
-  }
+// Add balls on click
+window.addEventListener('click', function (e) {
+  const ball = createBall();
+  balls.push(ball);
+});
 
+// Loop through all the balls and draw each one on the screen
+function loop() {
   ctx.fillStyle = 'rgba(255, 255, 255)';
   ctx.fillRect(0, 0, width, height);
 
-  for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
+  while (balls.length < 30) {
+    const ball = createBall();
+    balls.push(ball);
   }
+
+  balls.forEach(ball => {
+    ball.draw();
+    ball.updatePosition();
+  });
 
   requestAnimationFrame(loop);
 }
 
 loop();
-
-window.addEventListener('click', function (e) {
-  let ball = createBall();
-  balls.push(ball);
-});
