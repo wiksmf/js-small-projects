1bd613f8d12a9c665950c57599ce70e59687cc56
wiksmf <wiksmf@gmail.com>
2021-02-11 20:09:51 +0100

    html, css, js update

--- Building Blocks/main.js
+++ Building Blocks/main.js
@@ -1,39 +1,55 @@
 'use strict';
 
-const figures = document.querySelectorAll('div');
+const blockContainer = document.querySelector('.block-container');
 
-let initialX = 0;
-let initialY = 0;
-let currentX = 0;
-let currentY = 0;
-let xOffset = 0;
-let yOffset = 0;
+const shapes = [
+  'square',
+  'rectangle-up',
+  'rectangle-down',
+  'circle',
+  'semi-circle',
+  'quartright-circle',
+  'quartleft-circle',
+  'triangle',
+  'triangle-left',
+  'triangle-right',
+  'parallelogram-right',
+  'parallelogram-left',
+];
 
-let dragActive = false;
+let currentIndex = 1;
 
-figures.forEach((figure) => {
-  figure.addEventListener('mousedown', (e) => {
-    figure.classList.add('selected');
-    dragActive = true;
-    initialX = e.offsetX;
-    initialY = e.offsetY;
-  });
+// Create the blocks and append them to the container
+for (var i = 0; i < shapes.length; i++) {
+  for (var j = 0; j < 10; j++) {
+    const block = document.createElement('div');
+    block.classList.add('block', shapes[i]);
+    blockContainer.append(block);
+  }
+}
 
-  figure.addEventListener('mousemove', (e) => {
-    if (dragActive) {
-      currentX = e.clientX - initialX;
-      currentY = e.clientY - initialY;
-      updatePosition(figure, currentX, currentY);
+// Select all the available blocks
+document.querySelectorAll('.block').forEach(block => {
+  block.addEventListener('mousedown', e => {
+    const moveX = e.clientX - block.getBoundingClientRect().left;
+    const moveY = e.clientY - block.getBoundingClientRect().top;
+
+    // Assign a different z-index to each block when clicked
+    block.style.zIndex = currentIndex * 2;
+    currentIndex++;
+
+    // Move the selected block
+    document.addEventListener('mousemove', moveTo);
+
+    // Drop the block at the current mouse position
+    block.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', moveTo);
+    });
+
+    // Move the selected block
+    function moveTo(e) {
+      block.style.left = `${e.pageX - moveX}px`;
+      block.style.top = `${e.pageY - moveY}px`;
     }
   });
-
-  figure.addEventListener('mouseup', () => {
-    figure.classList.remove('selected');
-    dragActive = false;
-  });
 });
-
-function updatePosition(figure, currentX, currentY) {
-  figure.style.top = `${currentY}px`;
-  figure.style.left = `${currentX}px`;
-}
