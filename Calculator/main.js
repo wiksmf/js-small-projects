885d9602dce7cc30c9ee386dc003f9421ab8a387
wiksmf <wiksmf@gmail.com>
2021-02-14 22:42:25 +0100

    html, css, js update

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,32 +1,89 @@
 'use strict';
 
-const addBtn = document.querySelector('.addBtn');
-const subtractBtn = document.querySelector('.subtractBtn');
-const multiplyBtn = document.querySelector('.multiplyBtn');
-const divideBtn = document.querySelector('.divideBtn');
+const calculatorItems = document.querySelector('.calculator__items');
+const displayResult = document.querySelector('.calculator-display');
 
-const firstNumber = document.getElementById('firstNumber');
-const secondNumber = document.getElementById('secondNumber');
+let total, currentNumber, nextNumber, operator;
 
-const resultCalc = document.querySelector('.result');
+// Handle user's click on the calculator
+calculatorItems.addEventListener('click', e => {
+  if (!e.target.classList.contains('calculator__item')) return;
 
+  const item = e.target.getAttribute('data-input-value');
 
-addBtn.addEventListener('click', () => {
-  let result = (+firstNumber.value) + (+secondNumber.value);
-  resultCalc.textContent = result;
+  if (Number.isInteger(parseFloat(item))) displayInput(item);
+  if (item === '.') handelDecimal(item);
+  if (item === 'AC') init();
+  if (
+    item === '+' ||
+    item === '-' ||
+    item === '*' ||
+    item === '/' ||
+    item === '='
+  )
+    handleOperation(item);
+
+  displayResult.value = total;
 });
 
-subtractBtn.addEventListener('click', () => {
-  let result = (+firstNumber.value) - (+secondNumber.value);
-  resultCalc.textContent = result;
-});
+// Display the current clicked number
+function displayInput(number) {
+  if (nextNumber) {
+    total = number;
+    nextNumber = false;
+  } else {
+    total = total === '0' ? number : total + number;
+  }
+}
 
-multiplyBtn.addEventListener('click', () => {
-  let result = (+firstNumber.value) * (+secondNumber.value);
-  resultCalc.textContent = result;
-});
+// Handle decimal values
+function handelDecimal(decimal) {
+  if (!total.includes(decimal)) total += decimal;
 
-divideBtn.addEventListener('click', () => {
-  let result = (+firstNumber.value) / (+secondNumber.value);
-  resultCalc.textContent = result.toFixed(2);
-});
+  if (nextNumber) {
+    total = '0.';
+    nextNumber = false;
+    return;
+  }
+}
+
+// Handle the operations
+function handleOperation(nextOperator) {
+  const input = parseFloat(total);
+
+  if (operator && nextNumber) {
+    operator = nextOperator;
+    return;
+  }
+
+  if (!currentNumber && !isNaN(input)) currentNumber = input;
+
+  if (operator) {
+    const result = calculateTotal(currentNumber, input);
+    total = `${parseFloat(result.toFixed(5))}`;
+    currentNumber = result;
+  }
+
+  nextNumber = true;
+  operator = nextOperator;
+}
+
+// Calculate the total
+function calculateTotal(currentNumber, nextNumber) {
+  if (operator === '+') return currentNumber + nextNumber;
+  if (operator === '-') return currentNumber - nextNumber;
+  if (operator === '*') return currentNumber * nextNumber;
+  if (operator === '/') return currentNumber / nextNumber;
+
+  return nextNumber;
+}
+
+// Initialize all the values
+function init() {
+  total = '0';
+  currentNumber = null;
+  nextNumber = false;
+  operator = null;
+}
+
+init();
