b96e712c45277f8273c31c5ecc875143214403eb
wiksmf <wiksmf@gmail.com>
2021-02-16 22:01:06 +0100

    html, css, js update

--- Lottery Game/main.js
+++ Lottery Game/main.js
@@ -2,10 +2,11 @@
 
 const lottoContainer = document.querySelector('.lotto-container');
 const ballContainer = document.querySelector('.ball-container');
-const btnPlay = document.querySelector('.btn');
+const btnPlay = document.querySelector('.btn--play');
+const btnReset = document.querySelector('.btn--reset');
 
-const userNumbers = [];
-const lottoNumbers = [];
+let userNumbers = [],
+  lottoNumbers = [];
 
 // Create lotto grid cells and add them to the page
 for (let i = 1; i <= 42; i++) {
@@ -15,14 +16,73 @@ for (let i = 1; i <= 42; i++) {
   );
 }
 
-// Create lotto number balls and add them to the page
-for (let i = 0; i <= 5; i++) {
-  ballContainer.insertAdjacentHTML(
-    'beforeend',
-    `<div class="ball-number">
-      <span class="random-number" data-random-number="${i}"></span>
-    </div>`,
-  );
+// Track the numbers selected by the user
+function getUserNumber() {
+  if (userNumbers.length < 6) {
+    const number = this.dataset.number;
+
+    if (userNumbers.includes(+number)) return;
+
+    userNumbers.push(+number);
+
+    document
+      .querySelector(`[data-number="${number}"]`)
+      .classList.add('selected');
+  }
+  if (userNumbers.length === 6) btnPlay.disabled = false;
+}
+
+// Get random number and display them
+function getRandomNumbers() {
+  for (let i = 0; i < 6; i++) {
+    const randomNumber = Math.floor(Math.random() * 42 + 1);
+
+    if (lottoNumbers.includes(randomNumber)) {
+      i--;
+      continue;
+    }
+
+    ballContainer.insertAdjacentHTML(
+      'beforeend',
+      `<div class="ball-number">
+        <span data-random-number="${randomNumber}">${randomNumber}</span>
+      </div>`,
+    );
+
+    lottoNumbers.push(randomNumber);
+  }
+}
+
+// Check which number has been guessed
+function isSameNumber() {
+  userNumbers.filter(numb => {
+    if (lottoNumbers.includes(numb))
+      document
+        .querySelector(`[data-random-number="${numb}"]`)
+        .parentNode.classList.add('guessed');
+  });
+}
+
+// Play the game
+function playLotto() {
+  getRandomNumbers();
+  isSameNumber();
+  btnPlay.disabled = true;
+  btnReset.disabled = false;
+}
+
+// Reset all and restart the game
+function resetGame() {
+  document
+    .querySelectorAll(`[data-number]`)
+    .forEach(item => item.classList.remove('selected'));
+
+  document
+    .querySelectorAll('.ball-number')
+    .forEach(item => item.parentNode.removeChild(item));
+
+  userNumbers.length = lottoNumbers.length = 0;
+  btnReset.disabled = true;
 }
 
 // Event handlers
@@ -31,28 +91,4 @@ document.querySelectorAll('[data-number]').forEach(number => {
 });
 
 btnPlay.addEventListener('click', playLotto);
-
-// Track the numbers selected by the user
-function getUserNumber() {
-  if (userNumbers.length < 6) {
-    const number = this.dataset.number;
-    userNumbers.push(number);
-    document
-      .querySelector(`[data-number="${number}"]`)
-      .classList.add('selected');
-  }
-}
-
-// Play the game
-function playLotto() {
-  for (let i = 0; i < 6; i++) {
-    const randomNumber = Math.floor(Math.random() * 42 + 1);
-    if (randomNumber === lottoNumbers[i]) return getNumber();
-    document.querySelector(
-      `[data-random-number="${i}"]`,
-    ).textContent = randomNumber;
-    lottoNumbers.push(randomNumber);
-  }
-
-  btnPlay.textContent = 'play again';
-}
+btnReset.addEventListener('click', resetGame);
