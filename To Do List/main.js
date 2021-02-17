1f2819a476f3b8f0377ff01e644d266e9491ca34
wiksmf <wiksmf@gmail.com>
2021-02-17 23:10:24 +0100

    html, css, js update

--- To Do List/main.js
+++ To Do List/main.js
@@ -1,31 +1,38 @@
 'use strict';
 
-const taskNumber = document.querySelector('.tasks--number');
+const totalNumberTasks = document.querySelector('.total__tasks');
 const userInput = document.querySelector('.form__input');
 const addItem = document.querySelector('.btn--add');
-const listItems = document.querySelector('.list__item');
+const listTasks = document.querySelector('.list__tasks');
 const items = document.getElementsByClassName('task');
 
-addItem.addEventListener('click', createNewItem);
+addItem.addEventListener('click', addTask);
 
-function createNewItem(e) {
-  e.preventDefault();
+// Add a new task when clicking on the "Add" button
+function addTask(e) {
+  const newTask = document.createElement('li');
 
-  if (userInput.value === '') return;
+  if (!userInput.value) return;
 
-  const newItem = document.createElement('li');
-  newItem.className = 'task';
-  newItem.innerHTML = `${userInput.value}<button class="btn btn--delete">🗑️</button>`;
-  listItems.append(newItem);
+  newTask.className = 'task';
+  newTask.innerHTML = `${userInput.value}<button class="btn btn--delete">&times;</button>`;
+  listTasks.append(newTask);
+
+  newTask.querySelector('.btn--delete').addEventListener('click', removeTask);
+  newTask.addEventListener('click', checked);
+
+  totalNumberTasks.textContent = items.length;
   userInput.value = '';
-
-  console.log(listItems.length);
-
-  taskNumber.textContent = items.length;
-  newItem.querySelector('button').addEventListener('click', removeTask);
+  userInput.focus();
 }
 
+// Remove the selected task from the list
 function removeTask(e) {
   e.target.parentNode.remove();
   taskNumber.textContent = items.length;
+}
+
+// Add a checked symbol when clicking on a task
+function checked(e) {
+  if (e.target.tagName === 'LI') e.target.classList.toggle('checked');
 }
