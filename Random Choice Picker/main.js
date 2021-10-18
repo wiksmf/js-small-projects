bc3899f694495e4902555c9024612f280829fb28
wiksmf <wiksmf@gmail.com>
2021-10-18 20:52:52 +0200

    initial commit

---
+++ Random Choice Picker/main.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const tagsEl = document.querySelector('#tags');
+const textArea = document.querySelector('#textarea');
+
+textArea.focus();
+
+textArea.addEventListener('keyup', e => {
+  createTags(e.target.value);
+
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = '';
+    }, 10);
+    randomSelect();
+  }
+});
+
+function createTags(input) {
+  const tags = input
+    .split(',')
+    .filter(el => el.trim() !== '')
+    .map(el => el.trim());
+
+  tagsEl.innerHTML = '';
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
+
+function randomSelect() {
+  const times = 20;
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight');
+}
